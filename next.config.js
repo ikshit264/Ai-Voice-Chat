d01e133a9e4d8dd4d1dfa336b004c0e72d59d731
ikshit264 <ikshit.talera@gmail.com>
2025-06-04 00:51:20 +0530

    First commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["mongodb"],
-  },
+  serverExternalPackages: ["mongodb"],
+
   eslint: {
     ignoreDuringBuilds: true,
   },
